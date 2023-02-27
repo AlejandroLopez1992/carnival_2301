25dbf9e91ce28454344ed967936cbd244cc01df7
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-27 09:36:03 -0700

    Test: add testing for tall_enough? method

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -22,4 +22,11 @@ RSpec.describe Visitor do
   visitor1.add_preference(:water)
   expect(visitor1.preferences).to eq([:gentle, :water])
   end
+
+  it '#tall_enough? method verifies if visitor is tall enough' do
+  expect(visitor1.tall_enough?(54)).to eq(true)
+  expect(visitor2.tall_enough?(54)).to eq(false)
+  expect(visitor3.tall_enough?(54)).to eq(true)
+  expect(visitor1.tall_enough?)(64).to eq(false)
+  end
 end
