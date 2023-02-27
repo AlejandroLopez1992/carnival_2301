0ee57d78fea83ce12671563e7d1dfcef4b0139d1
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-27 10:00:23 -0700

    Test/Feat: add initial testing for board_rider method and condition for preferences

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,4 +1,20 @@
 class Ride
  
-  
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement,
+              :rider_log
+
+  def initialize(ride_info)
+    @name = ride_info[:name]
+    @min_height = ride_info[:min_height]
+    @admission_fee = ride_info[:admission_fee]
+    @excitement = ride_info[:excitement]
+    @rider_log = {}
+  end
+
+  def board_rider(visitor)
+    visitor.preferences.include?(@excitement)
+  end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe Ride do
   expect(ride1.admission_fee).to eq(1)
   expect(ride1.excitement).to eq(:gentle)
   end
+
+  it '#board_rider tracks visitors in rider_log' do
+  visitor1.add_preference(:gentle)
+  visitor2.add_preference(:gentle)
+
+  expect(ride1.board_rider(visitor1)).to eq(true)
+  # expect(ride1.rider_log).to eq({visitor1})
+  end
+
+  xit '#rider_log * admission is total_revenue' do
+  expect(ride1.total_revenue).to eq(0)
+  visitor1.add_preference(:gentle)
+  visitor2.add_preference(:gentle)
+
+  end
+  
 end
